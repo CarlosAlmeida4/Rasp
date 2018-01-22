0fea920073edd99b831896b7d724964374fa3995
Carlos <ceduardosa4@gmail.com>
2018-01-22 15:28:15 +0000

    GPS library addition

---
+++ GPS.hpp
@@ -0,0 +1,21 @@
+#include <iostream> // cout
+#include <sstream> // cout
+#include <stdlib.h>  // exit
+#include <string.h> // bzero
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+//user created
+#include "libgpsmm.h"
+
+
+/*
+ * We should get libgps_dump_state() from the client library, but
+ * scons has a bug; we can't get it to add -lgps to the link line,
+ * apparently because it doesn't honor parse_flags on a Program()
+ * build of a C++ file.
+ */
+static void libgps_dump_state(struct gps_data_t *);
