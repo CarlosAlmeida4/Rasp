1fdd84798a2d87b7f84d7ae60f0468d761d99fa7
Carlos <ceduardosa4@gmail.com>
2019-05-05 23:54:22 +0100

    Added Timer Library
    Added Timer capacity to the program, now it has a Timer library that allows multiple timed callbacks, example program can be found in the main for the time being and the header file contains the explanation for each function

--- Rasp_main.cpp
+++ Rasp_main.cpp
@@ -1,19 +1,19 @@
 /*
- * Rasp_TCP.cpp
- *
- *
- *	compilation command
- *	g++ Rasp_main.cpp -lm TCP_connections.o -lgps -o server
- *  g++ Rasp_main.cpp -lm TCP_connections.o -lgps -lpthread -o server
- *	g++ -c TCP_connections.cpp -o TCP_connections.o
- *	g++ -c GPS.cpp -o GPS.o
- *	//Full deployement on the Rasp
- *	g++ Rasp_main.cpp TCP_connections.cpp sensors.cpp rs232.c csv.cpp -lgps -lpthread -o server
- *  //Remote Testing
- *  g++ Rasp_main.cpp TCP_connections.cpp sensors.cpp csv.cpp -lpthread -o server
- *	cd /
- * 	cd mnt/c/Users/Carlos\ Almeida/Documents/Workspace
- */
+* Rasp_TCP.cpp
+*
+*
+*	compilation command
+*	g++ Rasp_main.cpp -lm TCP_connections.o -lgps -o server
+*  g++ Rasp_main.cpp -lm TCP_connections.o -lgps -lpthread -o server
+*	g++ -c TCP_connections.cpp -o TCP_connections.o
+*	g++ -c GPS.cpp -o GPS.o
+*	//Full deployement on the Rasp
+*	g++ Rasp_main.cpp TCP_connections.cpp sensors.cpp rs232.c csv.cpp -lgps -lpthread -o server
+*  //Remote Testing
+*  g++ Rasp_main.cpp TCP_connections.cpp sensors.cpp csv.cpp -lpthread -o server
+*	cd /
+* 	cd mnt/c/Users/Carlos\ Almeida/Documents/Workspace
+*/
 
 #include <iostream> // cout
 #include <sstream> // cout
@@ -34,44 +34,74 @@
 #include "TCP_connections.hpp"
 #include "sensors.hpp"
 #include "csv.hpp"
-
+#include "m_Timer.h"
 int port =8001;
 
 using namespace std;
 
+
+void time_handler1(size_t timer_id, void * user_data)
+{
+	printf("Single shot timer expired.(%d)\n", timer_id);
+}
+
+void time_handler2(size_t timer_id, void * user_data)
+{
+	printf("10 sec timer expired. (%d)\n", timer_id);
+}
+
+void time_handler3(size_t timer_id, void * user_data)
+{
+	printf("5 sec timer expired. (%d)\n", timer_id);
+}
+
+
 //main
 int main(int argc, char *argv[]){
 
-	  int server_socket;
-		int sensors_Flag=0;
+	int server_socket;
+	int sensors_Flag=0;
 
-		//initialize GPS client so that the gps is active
-		pthread_t sensor_thread;
-		pthread_create(&sensor_thread,NULL,client_sensors, &sensors_Flag);
+	//initialize GPS client so that the gps is active
+	pthread_t sensor_thread;
+	pthread_create(&sensor_thread,NULL,client_sensors, &sensors_Flag);
 
-		//Inicializa, must be initialized after gps is initialized
-		server_socket = tcp_init_server(port);
-		//Cria o thread para acompanhar o client_socket
-		pthread_t client_thread;
-		pthread_create(&client_thread, NULL, tcp_client_receiver, &server_socket);
-
-
-		csv_file csv_input;
-
-		csv_input.csv_file = csv_input.csv_open_file("Example_file1.csv");
-
-		char line[1024];
-		string str;
-		int i = 0;
+	//Inicializa, must be initialized after gps is initialized
+	server_socket = tcp_init_server(port);
+	//Cria o thread para acompanhar o client_socket
+	pthread_t client_thread;
+	pthread_create(&client_thread, NULL, tcp_client_receiver, &server_socket);
 
 
-		fclose(csv_input.csv_file);
+	csv_file csv_input;
+
+	csv_input.csv_file = csv_input.csv_open_file("Example_file1.csv");
+
+	char line[1024];
+	string str;
+	int i = 0;
 
 
-		// // uncomment to continue running
-		while(1);
+	fclose(csv_input.csv_file);
 
-		cout << "hi" << endl;
+	size_t timer1, timer2, timer3;
 
-	  return 0;
+	initialize();
+
+	timer1 = start_timer(20, time_handler1, TIMER_SINGLE_SHOT, NULL);
+	timer2 = start_timer(10, time_handler2, TIMER_PERIODIC, NULL);
+	timer3 = start_timer(5, time_handler3, TIMER_PERIODIC, NULL);
+
+	sleep(10);
+
+	stop_timer(timer1);
+	stop_timer(timer2);
+	stop_timer(timer3);
+
+	finalize();
+
+	// // uncomment to continue running
+	//while(1);
+
+	return 0;
 }
